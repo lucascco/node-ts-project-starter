00479889b050e441d85e23cf67bf4434c2c2f40f
Lucas C Correa <lucasccorrea@gmail.com>
2020-07-17 21:24:13 -0300

    up bae project

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.get('/', (req, res) => res.json({ message: 'Hello Word' }));
+
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333');
+});
